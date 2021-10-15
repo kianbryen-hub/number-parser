3a16f81003802b094b151461d713ddb723daa6ae
Kian Bryen <kianbryen@gmail.com>
2021-10-15 20:11:30 +0100

    Adding unit tests for sample user journeys.

---
+++ src/main/java/com/kb/numberparser/NumberParser.java
@@ -0,0 +1,16 @@
+package com.kb.numberparser;
+
+import java.util.Map;
+
+public class NumberParser {
+
+    public NumberParser(Map<String, Integer> countryCodes, Map<String, String> nationalTrunkPrefixes) {
+        // TODO - implement
+    }
+
+    public String parse(String dialledNumber, String userNumber) {
+        // TODO - implement
+        return null;
+    }
+
+}
---
+++ src/test/java/com/kb/numberparser/NumberParserTest.java
@@ -0,0 +1,34 @@
+package com.kb.numberparser;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Map;
+
+@SpringBootTest
+class NumberParserTest {
+
+    private static final NumberParser NUMBER_PARSER = new NumberParser(
+            Map.of("UK", 44, "FR", 33, "US", 1),
+            Map.of("UK", "0", "FR", "0", "US", "1")
+    );
+
+    @Test
+    void testParseUKtoUK() {
+        Assert.assertEquals("+447277822334",
+                NUMBER_PARSER.parse("07277822334", "+447866866886" ));
+    }
+
+    @Test
+    void testParseUStoUS() {
+        Assert.assertEquals("+1312233244",
+                NUMBER_PARSER.parse("1312233244", "+1212233200" ));
+    }
+
+    @Test
+    void testParseUKtoUS() {
+        Assert.assertEquals("+1212233200",
+                NUMBER_PARSER.parse("1312233244", "+447866866886" ));
+    }
+}
