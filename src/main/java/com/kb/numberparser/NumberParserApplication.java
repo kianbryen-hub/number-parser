62a03109d9976f86893bcc96d82b79eedb3935eb
Kian Bryen <kianbryen@gmail.com>
2021-10-15 18:25:56 +0100

    Initial project.

---
+++ src/main/java/com/kb/numberparser/NumberParserApplication.java
@@ -0,0 +1,13 @@
+package com.kb.numberparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kb/numberparser/NumberParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kb.numberparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
